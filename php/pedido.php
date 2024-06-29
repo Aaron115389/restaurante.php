42feeeb827553eb7137d85b0dcf4cfc8c83a9ba2
Daniel Moran Vilchez <113327439+DanielMoranV@users.noreply.github.com>
2024-06-29 00:14:35 -0500

    Diseño de interfas

--- php/pedido.php
+++ php/pedido.php
@@ -25,6 +25,52 @@ $iDbebidas = $_POST["cbxBebidas"];
 $cantidadbebidas = $_POST["txtCantidadBebidas"];
 $totalBebidas = $bebidas[$iDbebidas]['precio'] * $cantidadbebidas;
 
-echo $platos[$iDplato]['nombre'] . " : " . $cantidadPlato . " x s/. " . $platos[$iDplato]['precio'] . " =  " . $total;
-echo $bebidas[$iDbebidas]['nombre'] . " : " . $cantidadbebidas . " x s/. " . $bebidas[$iDbebidas]['precio'] . " =  " . $totalBebidas;
+//echo $platos[$iDplato]['nombre'] . " : " . $cantidadPlato . " x s/. " . $platos[$iDplato]['precio'] . " =  " . $total;
+//echo $bebidas[$iDbebidas]['nombre'] . " : " . $cantidadbebidas . " x s/. " . $bebidas[$iDbebidas]['precio'] . " =  " . $totalBebidas;
+?>
+<!DOCTYPE html>
+<html>
 
+<head>
+    <title>EL DELICIOSO | Pedido</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="Js.Js"></script>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+
+<body>
+    <main class="page payment-page">
+        <section class="payment-form dark">
+            <div class="container">
+                <div class="block-heading">
+                    <h2>¡BIENVENIDOS!</h2>
+                    <h2>Restaurante "El Delicioso"</h2>
+                    <p>Si te estás preguntando cuál es el amor más sincero… Entra y encontrarás la respuesta</p>
+                    <p> ¡Cualquier día es perfecto para darte un gusto!</p>
+                </div>
+                <div class="products">
+                    <h3 class="title">Detalle de Pedido</h3>
+                    <div class="item">
+
+                        <div class="form-group col-sm-8">
+                            <label for="cantidad-producto">
+                                <?php echo $platos[$iDplato]['nombre'] . " s/." . $platos[$iDplato]['precio'] . " x " . $cantidadPlato ?>
+                            </label>
+                            <input id="cantidad-producto" type="text" class="form-control" placeholder="Ingrese la cantidad" aria-label="Ingrese la cantidad" aria-describedby="basic-addon1" name="txtCantidadPlato" value="<?php echo $total; ?>" readonly>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
